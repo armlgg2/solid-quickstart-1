0b737f8518cb1d62c7ae7f4db4755af595213931
armlgg2 <111558337+armlgg2@users.noreply.github.com>
2025-05-30 16:04:13 +0700

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,10 +6,7 @@ export default function Home() {
   return (
     <section class="bg-slate-200 text-slate-700 p-8 rounded-md">
       <h2 class="text-2xl">บริษัท ไซเลนเทค จำกัด</h2>
-      <p class="mt-4">SILENTECH COMPANY LIMITED</p>
-
-   
-      
+      <p class="mt-4">SILENTECH COMPANY LIMITED</p>   
       </div>
     </section>
   );
