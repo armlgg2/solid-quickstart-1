b924b98a27dcfdf06981da25847ed25627f08303
armlgg2 <111558337+armlgg2@users.noreply.github.com>
2025-05-30 16:22:10 +0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -6,17 +6,7 @@ const App: Component = (props: { children: Element }) => {
 
   return (
     <div>
-      <nav class="bg-slate-100 text-slate-900 px-4">
-        <ul class="flex items-center">
-          <li class="py-2 px-4">
-            <A href="/" class="no-underline hover:underline">
-              Home
-            </A>
-          </li>
-          
-        </ul>
-      </nav>
-
+      
       <main class="max-w-2xl mx-auto h-screen flex flex-col justify-center px-4">
         <h1 class="text-5xl mx-auto mb-8 text-slate-600 font-light">Coming Soon!</h1>
         <Suspense>{props.children}</Suspense>
